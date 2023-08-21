0534345cb99d1b8e9e9f252d710be6ff5f543b8b
07hokage <saihaneesh.allu@gmail.com>
2023-08-21 07:20:02 -0500

    create custom packaage

---
+++ fetch_related/fetch_ws/build/m-explore/map_merge/CMakeFiles/combine_grids.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/saihaneesh/irvl/sem-exp/installations/fetch_related/fetch_ws/devel/lib/libcombine_grids.a"
+)
---
+++ fetch_related/fetch_ws/build/sem-map/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/saihaneesh/irvl/sem-exp/installations/fetch_related/fetch_ws/src/sem-map
+# Build directory: /home/saihaneesh/irvl/sem-exp/installations/fetch_related/fetch_ws/build/sem-map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
